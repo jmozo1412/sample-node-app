6560718eef7454960aeda85678281168fad82ea3
Siraj Rauff <siraj.rauff@indellient.com>
2018-11-05 19:47:03 -0500

    Adding port to config

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var app = require('./app');
 var http = require('http');
-var port = process.env.PORT || 8000;
+var nconf = require('nconf');
 
+nconf.file({ file: process.argv[2] || './dev-config.json' });
+
+var port = nconf.get('port');
 app.set('port', port);
 
 var server = http.createServer(app);
